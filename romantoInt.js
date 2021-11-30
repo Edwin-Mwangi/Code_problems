7305983d1dadda24a6eab38598e4df97f58c5c22
Edwin Mwangi <edwinmwangi138@gmail.com>
2021-11-30 15:47:44 +0300

    roman numbers to intergers

---
+++ romantoInt.js
@@ -0,0 +1,22 @@
+var romanToInt = function(s) {
+    const array =  ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+    const map = {
+        I : 1,
+        V : 5,
+        X : 10,
+        L : 50,
+        C : 100,
+        D : 500,
+        M : 1000
+    }
+
+    const convertMap = function(){
+        if(map.i > map(i-1)){
+            return map.i - map(i-1);
+        }else{
+            return map.i + map(i-1);
+        }
+
+    }
+
+};
