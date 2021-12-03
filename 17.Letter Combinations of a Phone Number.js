12fa7f165a846bc96a84feb3c4ed55672e4dfd4f
Edwin Mwangi <edwinmwangi138@gmail.com>
2021-12-03 17:50:13 +0300

    my incomplete working

---
+++ 17.Letter Combinations of a Phone Number.js
@@ -0,0 +1,25 @@
+var letterCombinations = function(digits) {
+
+    const lettersinInt = {
+        1 : ['a','b','c'],
+        3 :['d','e','f'],
+        4 : ['g','h','i'],
+        5 : ['j','k','l'],
+        6 : ['m','n','o'],
+        7 : ['p','q','r','s'],
+        8 : ['t','u','v'],
+        9 : ['w','x','y','z']
+    }
+
+    //separating individual digits in int
+    while(digits < 0){
+        digits.parseStr();
+        if(!digits.contains(1)){
+        
+        }
+
+    }
+
+    
+
+};
